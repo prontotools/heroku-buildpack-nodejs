17145b63ec65da685f796003a4da405735046391
Kanitkorn S <k.sujautra@gmail.com>
2016-09-29 07:30:07 +0700

    Reset everything :trollface:

--- profile/nodejs.sh
+++ profile/nodejs.sh
@@ -26,7 +26,6 @@ detect_memory() {
   esac
 }
 
-HOME="$HOME/survey"
 export PATH="$HOME/.heroku/node/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
 export NODE_HOME="$HOME/.heroku/node"
 export NODE_ENV=${NODE_ENV:-production}
